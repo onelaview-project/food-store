2acde5e5ee36a2876c6a8956c92d361681bace76
Nitipon Suratwisit <nitipon@gmail.com>
2025-04-08 18:37:25 +0700

    close redis in seed script

--- apps/api/src/seed/index.ts
+++ apps/api/src/seed/index.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '../app.module';
 import seedDiscountCampaigns from './seed-discount-campaign';
 import seedProducts from './seed-product';
+import Redis from 'ioredis';
 
 async function bootstrap() {
   const app = await NestFactory.createApplicationContext(AppModule);
@@ -9,6 +10,11 @@ async function bootstrap() {
   await seedDiscountCampaigns(app);
   await seedProducts(app);
   console.log('All seeding completed successfully');
+
+  // This is important to close the Redis connection to avoid app hanging
+  // though the seeding process doesn't involve Redis
+  const redis = app.get<Redis>('REDIS_CLIENT');
+  await redis.quit();
 
   await app.close();
 }
